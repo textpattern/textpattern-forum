7800102fe0a6eae85e544a9b3ba83a1bbfa79488
Jukka Svahn <web@rahinaa.biz>
2013-09-28 13:44:04 +0300

    Compass config.

---
+++ config.rb
@@ -0,0 +1,9 @@
+http_path = "./"
+project_path = "src"
+sass_path = "src/style"
+css_path = "tmp/style"
+images_dir = "img"
+fonts_dir = "fonts"
+output_style = :expanded
+environment = :production
+Sass::Script::Number.precision = 7
