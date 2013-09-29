264dc30f6f590cfe717d9eb0bcc504e3a356e21e
Jukka Svahn <web@rahinaa.biz>
2013-09-29 21:14:35 +0300

    Base JavaScript.

---
+++ src/style/Textpattern/js/main.js
@@ -0,0 +1,103 @@
+(function ()
+{
+    'use strict';
+
+    requirejs.config({
+        paths:
+        {
+            'jquery': '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min',
+            'webfont' : '//ajax.googleapis.com/ajax/libs/webfont/1.4.10/webfont'
+        },
+        shim:
+        {
+            'placeholder': ['jquery']
+        }
+    });
+
+    define('modernizr', function ()
+    {
+        return window.Modernizr;
+    });
+
+    // Placeholder polyfill.
+
+    require(['jquery'], function ($)
+    {
+        var placeholder = $('textarea[placeholder], input[placeholder]');
+
+        if (placeholder.length)
+        {
+            require(['placeholder'], function ()
+            {
+                placeholder.placeholder();
+            });
+        }
+    });
+
+    // Hack-fix for the iOS orientationchange zoom bug (NOTE: fixed in iOS 6).
+
+    require(['jquery', 'modernizr'], function ($, Modernizr)
+    {
+        if (Modernizr.touch)
+        {
+            var meta = $('meta[name=viewport]'), scales = [1, 1], fix = function ()
+            {
+                meta.attr('content', 'width=device-width,minimum-scale=' + scales[0] + ',maximum-scale=' + scales[1]);
+            };
+
+            fix();
+            scales = [0.25, 1.6];
+            $(document).one('gesturestart', fix);
+        }
+    });
+
+    // Test for SVG support via Modernizr, if yes then replace PNGs with SVGs.
+
+    require(['jquery', 'modernizr'], function ($, Modernizr)
+    {
+        if (Modernizr.svg)
+        {
+            $('img.svg').attr('src', function ()
+            {
+                return $(this).attr('src').replace('.png', '.svg');
+            });
+        }
+    });
+
+    // Responsive navigation.
+
+    require(['responsivenav'], function ()
+    {
+        responsiveNav('.nav-collapse', {
+            animate: true,
+            transition: 400,
+            label: 'Menu',
+            insert: 'before',
+            customToggle: '',
+            openPos: 'relative',
+            jsClass: 'js'
+        });
+    });
+
+    // Fonts.
+
+    require(['webfont'], function ()
+    {
+        WebFont.load({
+            google:
+            {
+                families: ['PT+Serif:400,700,400italic,700italic:latin', 'Cousine::latin']
+            }
+        });
+    });
+
+    // Analytics.
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-191562-10']);
+    _gaq.push(['_setDomainName', 'none']);
+    _gaq.push(['_gat._anonymizeIp']);
+    _gaq.push(['_trackPageview']);
+    require(['//www.google-analytics.com/ga.js']);
+
+})();
