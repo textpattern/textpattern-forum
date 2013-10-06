6e0593a9cc702465e43753d17f82b77b43e2fa4e
Jukka Svahn <web@rahinaa.biz>
2013-10-06 14:46:03 +0300

    Background clean up tasks.

---
+++ src/lib/Textpattern/Fluxbb/Tasks.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Textpattern\Fluxbb;
+use Textpattern\Fluxbb\Db;
+
+/**
+ * Runs maintenance tasks when requested.
+ *
+ * @example
+ * use Textpattern\Fluxbb\Tasks;
+ * new Tasks('aZdbY!daIoB');
+ */
+
+class Tasks
+{
+    /**
+     * Listened HTTP GET parameter.
+     *
+     * @var string
+     */
+
+    protected $parameter = 'textpattern_fluxbb_clean_inactive';
+
+    /**
+     * Constructor.
+     *
+     * @param string $key The key
+     */
+
+    public function __construct($key)
+    {
+        if (isset($_GET[$this->parameter]) && $_GET[$this->parameter] === $key)
+        {
+            foreach (get_class_methods($this) as $method)
+            {
+                if (strpos($method, 'task') === 0)
+                {
+                    $this->$method();
+                }
+            }
+
+            header('Content-Type: application/json; charset=utf-8');
+            echo json_encode(array('success' => true));
+            die();
+        }
+    }
+
+    /**
+     * Removes users older than a month and have never logged in.
+     */
+
+    public function taskCleanUserDatabase()
+    {
+        $time = strtotime('-1 month');
+        $deleted = Db::pdo()->exec("DELETE FROM users WHERE group_id = 0 and registered < {$time}");
+    }
+}
