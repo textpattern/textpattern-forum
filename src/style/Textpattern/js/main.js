50fcace69111795d95191da9d54ee958328a4774
Phil Wareham <phil@designhive.com>
2013-10-10 17:51:11 +0100

    Remove Cousine from monospace fontstack

--- src/style/Textpattern/js/main.js
+++ src/style/Textpattern/js/main.js
@@ -394,7 +394,7 @@
         WebFont.load({
             google:
             {
-                families: ['PT+Serif:400,700,400italic,700italic:latin', 'Cousine::latin']
+                families: ['PT+Serif:400,700,400italic,700italic:latin']
             }
         });
     });
