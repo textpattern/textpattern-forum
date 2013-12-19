6551ef0ddabbaf74d265153f66d909356d961315
Jukka Svahn <web@rahinaa.biz>
2013-12-19 12:12:22 +0200

    Update FluxBB version: fixes account renaming issue.

--- src/setup/setup.php
+++ src/setup/setup.php
@@ -32,48 +32,55 @@
  * This does the initial project setup.
  */
 
-$fluxbb_download = 'http://fluxbb.org/download/releases/1.5.5/fluxbb-1.5.5.tar.gz';
+$fluxbb_download = 'https://github.com/fluxbb/fluxbb/archive/00e6952aa8c2e0f5cd60028c531deb9dd801d77c.tar.gz';
 $fluxbb_dir = basename($fluxbb_download, '.tar.gz');
 
 `mkdir -pv tmp`;
-
 chdir('tmp');
-
-`rm -rf $fluxbb_dir`;
 `rm -rf fluxbb`;
+`mkdir -pv download`;
+chdir('download');
 
-if (!file_exists("$fluxbb_dir.tar.gz"))
-{
+if (!file_exists("$fluxbb_dir.tar.gz")) {
     echo "Downloading FluxBB...\n";
-    `curl -O $fluxbb_download`;
+    `curl -OL $fluxbb_download`;
 }
 
 echo "Extracting download...\n";
-`tar -zxvf $fluxbb_dir.tar.gz`;
-`mv $fluxbb_dir fluxbb`;
+`tar -xvf $fluxbb_dir.tar.gz`;
 
-chdir('fluxbb');
+$file = false;
+
+foreach (glob('*') as $file) {
+    if (is_dir($file) && preg_match('/[a-z0-9\-._]/i', $file)) {
+        $dir = basename($file);
+        break;
+    }
+}
+
+if (!$dir) {
+    echo 'Unable to find extracted directory.';
+}
+
+`mv $dir ../fluxbb`;
+chdir('../fluxbb');
 
 echo "Applying patches...\n";
 
-foreach (glob('../../src/setup/patches/*.patch') as $file)
-{
+foreach (glob('../../src/setup/patches/*.patch') as $file) {
     echo "Applying ".basename($file)."...\n";
     `patch -p1 < $file`;
 }
 
 echo "Cleaning up the downloaded package...\n";
 
-foreach (array('.git', '.gitattributes', '.gitignore', 'COPYING', 'readme.md', 'img') as $file)
-{
+foreach (array('.git', '.gitattributes', '.gitignore', 'COPYING', 'readme.md', 'img') as $file) {
     echo "Remove {$file}...\n";
     `rm -Rf '{$file}'`;
 }
 
-foreach (glob('style/*') as $file)
-{
-    if (basename($file) !== 'imports')
-    {
+foreach (glob('style/*') as $file) {
+    if (basename($file) !== 'imports') {
         echo "Remove {$file}...\n";
         `rm -Rf '{$file}'`;
     }
@@ -83,8 +90,7 @@ chdir('../../');
 
 // Keep existing configuration.
 
-if (file_exists('public/config.php'))
-{
+if (file_exists('public/config.php')) {
     echo "Keep existing config.php...\n";
     copy('public/config.php', 'tmp/fluxbb/config.php');
     echo "Remove install.php...\n";
@@ -93,12 +99,10 @@ if (file_exists('public/config.php'))
 
 echo "Moving in the new installation...\n";
 
-foreach (glob('public/*') as $file)
-{
+foreach (glob('public/*') as $file) {
     $name = basename($file);
 
-    if (!in_array($name, array('img', 'api')) && ($name === 'COPYING' || in_array(pathinfo($file, PATHINFO_EXTENSION), array('php', 'md', 'js')) || is_dir($file)))
-    {
+    if (!in_array($name, array('img', 'api')) && ($name === 'COPYING' || in_array(pathinfo($file, PATHINFO_EXTENSION), array('php', 'md', 'js')) || is_dir($file))) {
         echo "Remove {$file}...\n";
         `rm -Rf '{$file}'`;
     }
