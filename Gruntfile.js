7a31c3d030ee15b1746d90987471d5c040c8a9a5
Phil Wareham <phil@designhive.com>
2013-12-23 20:40:08 +0000

    Not using Web Font Loader anymore, remove from JSHint globals

--- Gruntfile.js
+++ Gruntfile.js
@@ -114,8 +114,7 @@ module.exports = function (grunt)
                     require: true,
                     requirejs: true,
                     responsiveNav: true,
-                    prettyPrint: true,
-                    WebFont: true
+                    prettyPrint: true
                 }
             }
         },
