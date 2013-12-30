bc5467f3b363c12ecaa0d1f90be449a2b0a1a0fa
Jukka Svahn <web@rahinaa.biz>
2013-12-30 19:32:03 +0200

    FluxBB uses HTTP only cookies.
    Can not read them from JavaScript; as should.

--- src/style/Textpattern/js/main.js
+++ src/style/Textpattern/js/main.js
@@ -574,7 +574,7 @@
 
     require(['jquery', 'cookie'], function ($)
     {
-        if (!$.cookie('acceptedCookies') && !$.cookie('textpattern-forum')) {
+        if (!$.cookie('acceptedCookies')) {
             var disclaimer = $('<aside id="cookie-disclaimer"><div class="container"><p><strong>This website uses cookies to enhance your experience.</strong> By continuing to use this website you agree to cookies being placed on your computer. If you wish to use this website but do not wish for cookies to be placed on your computer you can change the settings in your internet browser. <a href="#" data-action="close">Close</a>.</p></div></aside>');
             $('body').prepend(disclaimer);
             $.cookie('acceptedCookies', 1, {expires: 1461});
