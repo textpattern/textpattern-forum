3923ff1b8c9dc4e4bb9a27fd30debf16fe15caa4
Phil Wareham <phil@designhive.com>
2018-01-22 09:37:10 +0000

    jQuery v3.3.1

--- src/style/Textpattern/js/main.js
+++ src/style/Textpattern/js/main.js
@@ -6,7 +6,7 @@
 
     requirejs.config({
         paths: {
-            'jquery': 'https://code.jquery.com/jquery-3.2.1.min'
+            'jquery': 'https://code.jquery.com/jquery-3.3.1.min'
         }
     });
 
