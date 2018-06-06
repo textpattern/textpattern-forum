3ef2750b53c70328f3e7ddd8fa648a95c511af04
Phil Wareham <phil@designhive.com>
2018-06-06 14:17:13 +0100

    Update year to 2018

--- src/lib/Textpattern/Fluxbb/Db.php
+++ src/lib/Textpattern/Fluxbb/Db.php
@@ -8,7 +8,7 @@
  */
 
 /*
- * Copyright (C) 2017 Team Textpattern
+ * Copyright (C) 2018 Team Textpattern
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
