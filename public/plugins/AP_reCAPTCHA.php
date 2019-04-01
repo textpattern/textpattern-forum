770691d9947a9be18008200a425fb0c1f1bce461
Phil Wareham <phil@designhive.com>
2019-04-01 09:15:56 +0100

    Updated reCAPTCHA plugin

--- public/plugins/AP_reCAPTCHA.php
+++ public/plugins/AP_reCAPTCHA.php
@@ -13,14 +13,23 @@ if (!defined('PUN'))
 // Tell admin_loader.php that this is indeed a plugin and that it is loaded
 define('PUN_PLUGIN_LOADED', 1);
 
+// Load language file
+if (file_exists(PUN_ROOT.'lang/'.$pun_user['language'].'/recaptcha_addon.php'))
+    require PUN_ROOT.'lang/'.$pun_user['language'].'/recaptcha_addon.php';
+else
+    require PUN_ROOT.'lang/English/recaptcha_addon.php';
 
 // Store the config
 if (isset($_POST['process_form']))
 {
+    $enabled = isset($_POST['recaptcha_enabled']) ? 1 : 0;
     $site_key = isset($_POST['recaptcha_site_key']) ? pun_trim($_POST['recaptcha_site_key']) : '';
     $secret_key = isset($_POST['recaptcha_secret_key']) ? pun_trim($_POST['recaptcha_secret_key']) : '';
+    $location_register = isset($_POST['recaptcha_location_register']) ? 1 : 0;
+    $location_login = isset($_POST['recaptcha_location_login']) ? 1 : 0;
+    $location_guestpost = isset($_POST['recaptcha_location_guestpost']) ? 1 : 0;
 
-    foreach (compact('site_key', 'secret_key') as $key => $value)
+    foreach (compact('enabled', 'site_key', 'secret_key', 'location_register', 'location_login', 'location_guestpost') as $key => $value)
     {
         $key = 'recaptcha_'.$key;
 
@@ -36,7 +45,7 @@ if (isset($_POST['process_form']))
 
     generate_config_cache();
 
-    redirect('admin_loader.php?plugin=AP_reCAPTCHA.php', 'Settings saved successfully. Redirecting...');
+    redirect('admin_loader.php?plugin=AP_reCAPTCHA.php', $lang_recaptcha['Settings saved']);
 }
 
 
@@ -46,25 +55,31 @@ generate_admin_menu($plugin);
 ?>
 
 <div class="blockform">
-    <h2><span>reCAPTCHA</span></h2>
+    <h2><span><?= $lang_recaptcha['reCAPTCHA'] ?></span></h2>
     <div class="box">
         <form id="recaptcha" method="post" action="<?php echo $_SERVER['REQUEST_URI'] ?>">
             <div class="inform">
                 <fieldset>
-                    <legend>Configure reCAPTCHA integration</legend>
+                    <legend><?= $lang_recaptcha['General']; ?></legend>
                     <div class="infldset">
                         <p>
-                            Please provide your reCAPTCHA account credentials as provided by Google.
+                            <?= $lang_recaptcha['General description']; ?>
                         </p>
                         <table class="aligntop" cellspacing="0">
                             <tr>
-                                <th scope="row">Site key</th>
+                                <th scope="row"><?= $lang_recaptcha['Enable']; ?></th>
+                                <td>
+                                    <input type="checkbox" name="recaptcha_enabled" <?= empty($pun_config['recaptcha_enabled']) ? '' : 'checked' ?> />
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row"><?= $lang_recaptcha['Site key']; ?></th>
                                 <td>
                                     <input type="text" name="recaptcha_site_key" size="40" value="<?php if (!empty($pun_config['recaptcha_site_key'])) echo pun_htmlspecialchars($pun_config['recaptcha_site_key']); ?>" />
                                 </td>
                             </tr>
                             <tr>
-                                <th scope="row">Secret key</th>
+                                <th scope="row"><?= $lang_recaptcha['Secret key']; ?></th>
                                 <td>
                                     <input type="text" name="recaptcha_secret_key" size="40" value="<?php if (!empty($pun_config['recaptcha_secret_key'])) echo pun_htmlspecialchars($pun_config['recaptcha_secret_key']); ?>" />
                                 </td>
@@ -72,8 +87,37 @@ generate_admin_menu($plugin);
                         </table>
                     </div>
                 </fieldset>
+
+                <fieldset>
+                    <legend><?= $lang_recaptcha['Locations']; ?></legend>
+                    <div class="infldset">
+                        <p>
+                            <?= $lang_recaptcha['Locations description']; ?>
+                        </p>
+                        <table class="aligntop" cellspacing="0">
+                            <tr>
+                                <th scope="row"><?= $lang_recaptcha['Register']; ?></th>
+                                <td>
+                                    <input type="checkbox" name="recaptcha_location_register" <?= empty($pun_config['recaptcha_location_register']) ? '' : 'checked' ?> />
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row"><?= $lang_recaptcha['Login']; ?></th>
+                                <td>
+                                    <input type="checkbox" name="recaptcha_location_login" <?= empty($pun_config['recaptcha_location_login']) ? '' : 'checked' ?> />
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row"><?= $lang_recaptcha['Guest post']; ?></th>
+                                <td>
+                                    <input type="checkbox" name="recaptcha_location_guestpost" <?= empty($pun_config['recaptcha_location_guestpost']) ? '' : 'checked' ?> />
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </fieldset>
             </div>
-            <p class="submitend"><input type="submit" name="process_form" value="Save" /></p>
+            <p class="submitend"><input type="submit" name="process_form" value="<?= $lang_recaptcha['Save'] ?>" /></p>
         </form>
     </div>
 </div>
